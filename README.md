2e151f3d5a7fd0b8f291067b117de68a4c58f8a2
Adam746 <amweinbaum@gmail.com>
2026-01-24 13:17:47 -0500

    Added Kafka UI and test topics

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ docker-compose up -d
 ./scripts/init-topics.sh
 
 mvn exec:java
+
+To list existing topics
+docker exec broker-3.8 kafka-topics --list --bootstrap-server localhost:9092
+
+To completely reset broker and delete kafka topics
+docker exec broker-3.8 sh -c "kafka-topics --list --bootstrap-server localhost:9092 | grep 'streams-app' | xargs -I {} kafka-topics --delete --topic {} --bootstrap-server localhost:9092"
--- scripts/init-topics.sh
+++ scripts/init-topics.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# Create the topics required for the practice
-docker exec broker-3.8 kafka-topics.sh --create --topic input-topic --bootstrap-server localhost:9092 --if-not-exists
-docker exec broker-3.8 kafka-topics.sh --create --topic output-topic --bootstrap-server localhost:9092 --if-not-exists
+cub kafka-ready -b broker:29092 1 30
+
+kafka-topics --create --if-not-exists --bootstrap-server broker:29092 --partitions 1 --replication-factor 1 --topic practice-input
+kafka-topics --create --if-not-exists --bootstrap-server broker:29092 --partitions 1 --replication-factor 1 --topic practice-output
+
+echo "Topics created successfully!"
